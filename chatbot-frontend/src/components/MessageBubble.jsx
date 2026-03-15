59ce417cd1ae0bbaa12a6f25fdc5f18b68f43da4
Kamalesh <kamaleshbabu622@gmail.com>
2026-03-15 15:03:54 +0530

    Add features for UI

--- chatbot-frontend/src/components/MessageBubble.jsx
+++ chatbot-frontend/src/components/MessageBubble.jsx
@@ -1,14 +1,18 @@
 function MessageBubble({ message, sender }) {
-    const isUser = sender === "user";
+  const isUser = sender === "user";
 
-    return(
-        <div className = {`flex ${isUser ? "justify-end" : "justify-start"} mb-3`}>
-            <div className = {`px-4 py-2 rounded-xl max-w-[75%] sm:max-w-[60%] md:max-w-[50%] text-sm sm:text-base
-                 ${isUser ? "bg-blue-400" : "bg-gray-600"}`}>
-                {message}
-            </div>
-        </div>
-    );
+  return (
+    <div className={`flex ${isUser ? "justify-end" : "justify-start"} mb-3`}>
+      <div
+        className={`px-3 py-2 sm:px-4 sm:py-2 rounded-xl 
+          max-w-[85%] sm:max-w-[70%] md:max-w-[60%] lg:max-w-[50%] 
+          text-sm sm:text-base leading-relaxed
+          ${isUser ? "bg-blue-500" : "bg-zinc-700"}`}
+      >
+        {message}
+      </div>
+    </div>
+  );
 }
 
 export default MessageBubble;
